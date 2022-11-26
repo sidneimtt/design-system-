d64224d571995d5c26cbfffa0b815cf1b9b8ef8e
Sidnei <sidneim@hotmail.com>
2022-11-25 23:17:29 -0300

    replace package nov

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,7 +1,14 @@
 //confg. themes  Dark
-
 import { themes } from '@storybook/theming'
+import { initialize, mswDecorator } from 'msw-storybook-addon'
 import '../src/styles/global.css'
+
+//Initialize MSW
+initialize({
+  onUnhandledRequest: 'bypass',
+})
+//Provide the MSW addon decorator globally
+export const decorators = [mswDecorator]
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
